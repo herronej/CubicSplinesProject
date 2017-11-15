1ab4b1ec0316e71620dd20e579ede203420a6af3
Emily Herron <emily.joyce.herron@live.mercer.edu>
2017-11-14 19:09:51 -0500

    added get peaks function

--- main.cpp
+++ main.cpp
@@ -5,6 +5,64 @@
 #include <string>
 using namespace std;
 
+void get_peaks(double x[1329], double a[1329]){
+
+    double y_prev = a[0];
+
+    for(int i = 1; i < 1329; i++){
+        if(y_prev < 0 and a[i] > 0)
+            cout << "start between " << x[i-1] << " and " << x[i] << endl;
+        if(y_prev > 0 and a[i] < 0)
+            cout << "end between " << x[i-1] << " and " << x[i] << endl;
+        y_prev = a[i];
+    }
+
+}
+
+/*
+double bisection(double a, double b){
+
+    cout << "Bisection Algorithm" << endl;
+
+    int n = 1;
+
+    double fa = f_x(a);
+
+    cout << "Iteration\t" << "Value of x\t" << "Value of f(x)\t" << "Absolute Error\t" << "Relative Error" <<  endl;
+
+    while( n <= 100 ){
+        double p = a + (b-a)/2.0;
+
+        cout << n << "\t";
+
+        printf("%15f\t", p);
+
+        double fp = f_x(p);
+
+        double abs_err = abs(truth - p);
+
+        printf("%15f\t%15f\t%15f\n", fp, abs_err, abs_err/abs(truth));
+
+        if(fp == 0 || (b-a)/2.0 < TOL){
+            return p;
+        }
+
+        if (fa*fp > 0){
+            a = p;
+            fa = fp;
+        }
+
+        else{
+            b = p;
+        }
+
+        n += 1;
+     }
+
+     return NULL;
+}
+*/
+
 void sg_filter(int nFilterPoints, int nPasses, double x[1329], double a[1329]){
 
     double c5[5] = {-3, 12, 17, 12, -3};
@@ -15,7 +73,7 @@ void sg_filter(int nFilterPoints, int nPasses, double x[1329], double a[1329]){
         for(int i = (nFilterPoints-1)/2; i < 1329-(nFilterPoints-1)/2; i++){
             double sumX = 0.0;
             double sumA = 0.0;
-            for(int j = 0; j <= nFilterPoints; j++){
+            for(int j = 0; j < nFilterPoints; j++){
                 
                 if(nFilterPoints == 5){
                     sumA += c5[j]*a[i+1];
@@ -30,9 +88,10 @@ void sg_filter(int nFilterPoints, int nPasses, double x[1329], double a[1329]){
                 }
                 //sumA += a[j];
             }
-            if (p == nPasses)
-                cout << x[i] << "\t" << sumA/nFilterPoints << endl;
-        }
+            //if (p == nPasses)
+                //cout << x[i] << "\t" << sumA/nFilterPoints << endl;
+                a[i] = sumA/nFilterPoints;    
+    }
 
 }
 
@@ -40,14 +99,16 @@ void sg_filter(int nFilterPoints, int nPasses, double x[1329], double a[1329]){
 void boxcar_filter(int nFilterPoints, int nPasses, double x[1329], double a[1329]){
     
    for(int m = 0; m < nPasses; m++)
-    for(int i = 0; i < 1329; i++){
+    for(int i = (nFilterPoints-1)/2; i < 1329-(nFilterPoints-1)/2; i++){
         double sumX = 0.0;
         double sumA = 0.0;
         for(int j = i - (nFilterPoints-1)/2; j <= i + (nFilterPoints-1)/2; j++){
             sumA += a[j];
         }
-        if (m == 4)
-            cout << x[i] << "\t" << sumA/nFilterPoints << endl;
+        //if (m == 4)
+            //cout << x[i] << "\t" << sumA/nFilterPoints << endl;
+        a[i] = sumA/nFilterPoints;
+
     }
     
 
@@ -94,6 +155,8 @@ void getCoefficients(int n, double x[1329], double a[1329], double b[1329], doub
 
 int main(){
 
+    int baseline = 50;
+
     int n = 1328;
 
     int r = 0;
@@ -114,6 +177,11 @@ int main(){
         a[r] = ypt;
     }
 
+    // subtract baseline
+    for(int i = 0; i < 1329; i++)
+        a[i] -= baseline;
+
+
     double h[n+1];
     double alpha[n+1];
 
@@ -124,34 +192,6 @@ int main(){
     double c[n+1];
     double d[n+1];    
 
-    /*for(int i = 0; i < n; i++){
-        h[i] = x[i+1] - x[i];
-    }
-
-    for(int i = 1; i < n; i++){
-        alpha[i] = (3.0/h[i])*(a[i+1]-a[i]) - (3.0/h[i-1])*(a[i]-a[i-1]);
-    }
-
-    l[0] = 1.0;
-    mu[0] = 0.0;
-    z[0] = 0.0;
-
-    for(int i = 1; i < n; i++){
-        l[i] = 2.0*(x[i+1] - x[i-1])-h[i-1]*mu[i-1];
-        mu[i] = h[i]/l[i];
-        z[i] = (alpha[i] - h[i-1]*z[i-1])/l[i];
-    }
-
-    l[n] = 1.0;
-    z[n] = 0.0;
-    c[n] = 0.0;
-
-    for(int j = n-1; j>= 0; j -= 1){
-        c[j] = z[j] - mu[j]*c[j+1];
-        b[j] = (a[j+1]-a[j])/h[j] - h[j]*(c[j+1]+2*c[j])/3.0;
-        d[j] = (c[j+1]-c[j])/(3*h[j]);
-    }*/ 
- 
     getCoefficients(n, x, a, b, c, d);
 
     for(int j = 0; j < n; j++){
@@ -161,6 +201,8 @@ int main(){
     double currX = x[n];// x[0];
     int partIndex;    
 
+    
+    sg_filter(17, 5, x, a);
 
     // generate points
     for(int i = 1; i <= 10000; i++){
@@ -176,14 +218,16 @@ int main(){
 
         double total_y = a[partIndex] + b[partIndex]*(currX-x[partIndex]) + c[partIndex]*pow((currX-x[partIndex]),2.0) + d[partIndex]*pow((currX-x[partIndex]), 3.0);
 
-        //cout << currX << "\t" << total_y << endl;
+        cout << currX << "\t" << total_y << endl;
                 
 
         currX = currX - (x[n]-x[0])/10000.0;
         
     }
 
+    get_peaks(x, a);
+
     //boxcar_filter(5, x, a);
  
-    sg_filter(17, 5, x, a);   
+    //sg_filter(17, 5, x, a);   
 }
